ee348be6c01644ae040631488396c8fdd0f6e1d4
Mark Modak <mmodak@mmodak.local>
2018-12-03 18:12:11 -0700

    Full functionality for it2 (including 80 char limit) and refactored tests.

--- lib/night_writer_runner.rb
+++ lib/night_writer_runner.rb
@@ -2,11 +2,14 @@ require_relative 'text'
 
   text = Text.new(open("#{ARGV[0]}", &:read).chomp)
   text.braille(text.string)
-  char_count = text.string.each_char.count
+  char_count = text.string.length
 
   out_file = File.new("#{ARGV[1]}", "w")
-  out_file.write(text.str1 + "\n")
-  out_file.write(text.str2 + "\n")
-  out_file.write(text.str3 + "\n")
 
-  puts "Created #{ARGV[1]} containing #{char_count} characters"
+  until text.str1.length == 0
+    out_file.write(text.str1.slice!(0..79) + "\n")
+    out_file.write(text.str2.slice!(0..79) + "\n")
+    out_file.write(text.str3.slice!(0..79) + "\n")
+  end
+
+puts "Created #{ARGV[1]} containing #{char_count} characters."
--- test/test_night_writer.rb
+++ test/test_night_writer.rb
@@ -11,33 +11,17 @@ class WriterTest < Minitest::Test
   end
 
   def test_to_braille_array_for_string
-    assert_equal ["0.","00",".."], @new_text.to_braille("h")
+    @new_text.braille("h")
+    assert_equal "0.", @new_text.str1
   end
 
   def test_to_braille_array_for_string_of_an_instance
-    assert_equal ["0.","00",".."], @new_text.to_braille(@new_text.string)
+    @new_text.braille(@new_text.string)
+    assert_equal "0.", @new_text.str1
   end
 
   def test_to_output_with_hardcoded_array
     @new_text.to_output(["0.","00",".."])
     assert_equal @new_text.str1, "0."
-  end
-
-  def test_to_output_with_instance
-    array = @new_text.to_braille(@new_text.string)
-    @new_text.to_output(array)
-    assert_equal @new_text.str1, "0."
-  end
-
-  def test_braille_enumerate_over_two_characters
-    this_text = Text.new("hh")
-    this_text.braille("hh")
-    assert_equal this_text.str1, "0.0."
-  end
-
-  def test_braille_more_characters_from_the_object
-    this_text = Text.new("how you")
-    this_text.braille(this_text.string)
-    assert_equal this_text.str1, "0.0..0..000.0."
   end
 end
